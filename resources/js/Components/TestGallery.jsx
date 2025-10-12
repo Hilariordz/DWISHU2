969b66a2299edc63e4361e253bd25d29402fd930
Hilariordz <hilariordz936@gmail.com>
2025-10-12 12:49:40 -0600

    first commit

---
+++ resources/js/Components/TestGallery.jsx
@@ -0,0 +1,18 @@
+export default function TestGallery() {
+    return (
+        <div style={{ 
+            width: '100%', 
+            height: '400px', 
+            background: 'linear-gradient(45deg, #ff6b6b, #4ecdc4)',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: '20px',
+            color: 'white',
+            fontSize: '24px',
+            fontWeight: 'bold'
+        }}>
+            🎨 DomeGallery Test Component 🎨
+        </div>
+    );
+}
---
+++ resources/js/Pages/Gallery.jsx
@@ -0,0 +1,9 @@
+import DomeGallery from '../Components/DomeGallery';
+
+export default function Gallery() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <DomeGallery />
+    </div>
+  );
+}
